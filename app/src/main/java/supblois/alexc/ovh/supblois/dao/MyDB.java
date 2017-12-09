7937c97475f3f6e87d4d6712f7906ac5b5659cbe
Ben Crulis <bencrulis@gmail.com>
2017-12-09 15:36:33 +0100

    started working on DAO pattern

--- app/src/main/java/supblois/alexc/ovh/supblois/dao/MyDB.java
+++ app/src/main/java/supblois/alexc/ovh/supblois/dao/MyDB.java
@@ -9,44 +9,76 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 
 public class MyDB extends SQLiteOpenHelper {
-        //Table
-        public static final String TABLE_ACCOUNT = "RegAccount";
-        public static final String TABLE_MSG = "Messages";
-        public static final String TABLE_CONNECTION = "Connection";
+    //Tables
+    public static final String TABLE_ACCOUNT = "RegAccount";
+    public static final String TABLE_MSG = "Messages";
+    public static final String TABLE_CONNECTION = "Connected";
 
-        //column
-        public static final String COLUMN_NUM = "num";
-        public static final String COLUMN_FIRSTNAME = "firstName";
-        public static final String COLUMN_LASTNAME = "lastName";
-        //Database info
-        private static final String DATABASE_NAME = "messager.db";
-        private static final int DATABASE_VERSION = 1;
-        //Creation table request
-        private static final String DATABASE_CREATE = "create table "
-                        +
-                        TABLE_ACCOUNT
-                        +
-                        "("
-                        +
-                        COLUMN_NUM
-                        +
-                        COLUMN_FIRSTNAME
-                        +
-                        COLUMN_LASTNAME
-                        +
-                        ");";
+    //columns
+    public static final String COLUMN_NUM = "num";
+    public static final String COLUMN_FIRSTNAME = "firstName";
+    public static final String COLUMN_LASTNAME = "lastName";
 
-        //Init of sqlhelper
-        public MyDB(Context context) {
-            super(context
-                            ,
-                            DATABASE_NAME
-                            ,
-                            null
-                            ,
-                            DATABASE_VERSION
-                    );
-        }
+    public static final String COLUMN_MSG_ID = "id";
+    public static final String COLUMN_SENDER = "sender";
+    public static final String COLUMN_DATE = "date";
+    public static final String COLUMN_CONTENT = "content";
+
+    public static final String COLUMN_LOGGED_NUM = "num";
+    public static final String COLUMN_LOGGED_PSWD = "password";
+
+    //Database info
+    private static final String DATABASE_NAME = "messager.db";
+    private static final int DATABASE_VERSION = 1;
+
+    //Creation table request
+
+    public static final String NOTNULL = " NOT NULL ";
+
+    private static final String CREATE_TABLE_ACCOUNT = "create table "
+            +
+            TABLE_ACCOUNT
+            +
+            "("
+            +
+            COLUMN_NUM + "Text" + NOTNULL
+            +
+            COLUMN_FIRSTNAME
+            +
+            COLUMN_LASTNAME
+            +
+            ");";
+
+    private static final String CREATE_TABLE_MSG = "create table "
+            +
+            TABLE_MSG
+            +
+            "("
+            +
+            COLUMN_MSG_ID
+            +
+            COLUMN_SENDER
+            +
+            COLUMN_DATE
+            +
+            COLUMN_CONTENT
+            +
+            ");";
+
+    private static final String DATABASE_CREATE = CREATE_TABLE_ACCOUNT + "\n" +
+            CREATE_TABLE_MSG;
+
+    //Init of sqlhelper
+    public MyDB(Context context) {
+        super(context
+                ,
+                DATABASE_NAME
+                ,
+                null
+                ,
+                DATABASE_VERSION
+        );
+    }
 
     @Override
     public void
@@ -58,8 +90,8 @@ public class MyDB extends SQLiteOpenHelper {
     public void
     onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS "
-                                +
-                                TABLE_ACCOUNT);
+                +
+                TABLE_ACCOUNT);
         onCreate(db);
     }
 }
